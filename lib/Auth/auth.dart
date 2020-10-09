a743f4effdffc714885b944d994285116b2968b9
Yuu <yunhan.xia@epitech.eu>
2020-10-09 17:06:07 +0200

    Beginning of Login, Register Form...

---
+++ lib/Auth/auth.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: TextFormField(
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please enter your ID';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: TextFormField(
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: ButtonTheme(
+              minWidth: 250,
+              height: 50,
+              child: RaisedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context).showSnackBar(
+                        const SnackBar(content: Text('Logged in')));
+                  }
+                },
+                color: const Color(0xff132225),
+                textColor: const Color(0xffc89b3c),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    side: const BorderSide(color: Color(0xffc89b3c))),
+                child: const Text(
+                  'Submit',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Friz Quadrata',
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Scaffold scaffold = Scaffold(
+      backgroundColor: const Color(0xff132225),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Sign in',
+                style: TextStyle(
+                    fontFamily: 'Friz Quadrata',
+                    fontSize: 50,
+                    color: Color(0xffc89b3c)),
+              ),
+              LoginForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+    return scaffold;
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: TextFormField(
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please enter an ID';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: TextFormField(
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please enter a password';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: TextFormField(
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please confirm your password';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: ButtonTheme(
+              minWidth: 250,
+              height: 50,
+              child: RaisedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                color: const Color(0xff132225),
+                textColor: const Color(0xffc89b3c),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    side: const BorderSide(color: Color(0xffc89b3c))),
+                child: const Text(
+                  'Submit',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Friz Quadrata',
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Register extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Scaffold scaffold = Scaffold(
+      backgroundColor: const Color(0xff132225),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Register',
+                  style: TextStyle(
+                      fontFamily: 'Friz Quadrata',
+                      fontSize: 50,
+                      color: Color(0xffc89b3c)),
+                ),
+                RegisterForm()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    return scaffold;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lol_stats/Auth/auth.dart';
+import 'package:gradient_text/gradient_text.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,30 +17,91 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    var scaffold = Scaffold(
-      backgroundColor: Colors.teal,
+    final Scaffold scaffold = Scaffold(
+      backgroundColor: const Color(0xff132225),
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('assets/lol_logo.png'),
-              ),
-              Text(
+            children: <Widget>[
+              GradientText(
                 'Lol Stats',
-                style: TextStyle(
-                    fontFamily: 'Friz Quadrata',
-                    fontSize: 50,
-                    fontStyle: FontStyle.normal,
-                    color: Colors.red),
+                gradient: const LinearGradient(
+                    colors: <Color>[
+                      Color(0xff785a28),
+                      Color(0xffdeca7b),
+                      Color(0xff785a28)
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: <double>[0.25, 0.5, 0.75]),
+                style: const TextStyle(
+                  fontFamily: 'Friz Quadrata',
+                  fontSize: 80,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 250,
+                    height: 50,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push<dynamic>(
+                          context,
+                          MaterialPageRoute<dynamic>(
+                              builder: (BuildContext context) => Login()),
+                        );
+                      },
+                      color: const Color(0xff132225),
+                      textColor: const Color(0xffc89b3c),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          side: const BorderSide(color: Color(0xffc89b3c))),
+                      child: const Text(
+                        'SIGN IN',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Friz Quadrata',
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  ButtonTheme(
+                    minWidth: 250,
+                    height: 50,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push<dynamic>(
+                          context,
+                          MaterialPageRoute<dynamic>(
+                              builder: (BuildContext context) => Register()),
+                        );
+                      },
+                      color: const Color(0xff132225),
+                      textColor: const Color(0xffc89b3c),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          side: const BorderSide(color: Color(0xffc89b3c))),
+                      child: const Text(
+                        'CREATE ACCOUNT',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Friz Quadrata',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
